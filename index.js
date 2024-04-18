c2970a78a3768658aaa9965ddd07e0ac6326d380
Stephen Ochieng <materiamystic@gmail.com>
2024-04-18 19:40:14 +0300

    Adds the required static methods

--- index.js
+++ index.js
@@ -1,3 +1,40 @@
 class Formatter {
-  //add static methods here
+  static capitalize(name) {
+    return name.charAt(0).toUpperCase() + name.substring(1);
+  }
+
+  static sanitize(name) {
+    return name.replace(/[^a-zA-Z0-9-' ]/g, "");
+  }
+
+  static titleize(name) {
+    const excludeWords = [
+      "the",
+      "a",
+      "an",
+      "but",
+      "of",
+      "and",
+      "for",
+      "at",
+      "by",
+      "from",
+    ];
+    const words = name.split(" ");
+
+    if (words[0] === "a") {
+      words[0] = words[0].toUpperCase();
+    }
+
+    const titledWords = words.map((word) => {
+      if (!excludeWords.includes(word)) {
+        word = this.capitalize(word);
+        return word;
+      } else {
+        return word;
+      }
+    });
+
+    return titledWords.join(" ");
+  }
 }
